3326c5ec56d9eeba73730db660e042c39f102974
faraazcc <48faraaz@gmail.com>
2017-07-11 13:33:14 +0700

    Create app.py

--- app.py
+++ app.py
@@ -45,3 +45,26 @@ def handle_message(event):
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+	# hadist
+            if '/hd' in msg:
+                arg = msg.split('-');
+                if len(arg) > 1:
+                    imam = arg[0][3:].strip();
+                    nom = arg[1].strip();
+                    x='wget http://hadits.stiba.ac.id/\?imam\='+imam+'\&no\='+nom+'\&type\=hadits -qO - | awk \''+'BEGIN{IGNORECASE=1;FS=\"<div class=\\"ja-newsitem\\"'+' style=\\"width: 100%;\\">|</div>";RS=EOF} {print $13}\''
+                    proc=subprocess.Popen(x, shell=True, stdout=subprocess.PIPE) 
+                    xx=proc.communicate()[0].strip();
+                    pr = xx.replace('<p class="arab">','')
+                    pr = pr.replace('<p class="indo">','')
+                    pr = pr.replace('<h3>Terjemahan</h3>','Terjemahan : ')
+                    pr = pr.replace('</p>','')
+                    pr = pr.replace('<p>','')
+                    if len(pr) == 51:
+                        receiver.sendMessage('Not Found')
+                    else:
+                        receiver.sendMessage(pr)
+                else :
+                    helps  = '== help ==\n'
+                    helps += '[*]tirmidzi => (At Tirmidzi) \n[*]abudaud => (Abu Daud)\n[*]ahmad => (Ahmad)\n[*]bukhari => (Bukhari)\n[*]darimi => (Ad Darimi)\n[*]ibnumajah => (Ibnu Majah) \n[*]malik => (Malik)\n[*]muslim => (Muslim)\n[*]nasai => (An Nasa\'i)\n\ncontoh: /hd darimi-63\n\n* mencari hadist imam (ad darimi) nomer 63'
+                    receiver.sendMessage(helps)
